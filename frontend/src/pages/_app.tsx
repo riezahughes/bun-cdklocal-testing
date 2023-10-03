eb5105bf502630d248650921a25460bedf711f56
dan ruxton <dan@tantalus-troupe.com>
2023-10-03 12:43:53 +0200

    workign frontend

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
+
+const MyApp = ({ Component, pageProps }:any)  => {
+   return ( <ChakraProvider>
+        <Component {...pageProps}/>
+    </ChakraProvider>
+   )
+}
+
+export default MyApp
